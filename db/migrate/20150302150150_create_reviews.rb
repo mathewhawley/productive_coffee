3e8e69b0a09ac1776435b88c8be533b76cca44ad
Colin Durbin <cddurbin@gmail.com>
2015-03-02 15:04:05 +0000

    Add review model.

---
+++ db/migrate/20150302150150_create_reviews.rb
@@ -0,0 +1,18 @@
+class CreateReviews < ActiveRecord::Migration
+  def change
+    create_table :reviews do |t|
+      t.integer :power
+      t.integer :wifi
+      t.integer :coffee
+      t.integer :food
+      t.integer :service
+      t.integer :noise_level
+      t.text :content
+      t.integer :overall_rating
+      t.integer :user_id
+      t.integer :establishment_id
+
+      t.timestamps null: false
+    end
+  end
+end
