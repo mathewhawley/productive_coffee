75be46c313dc43c08c84afc44c067c468a1ca961
Colin Durbin <cddurbin@gmail.com>
2015-03-02 18:57:22 +0000

    Add reviews for establishment show page via backend.

--- app/controllers/establishments_controller.rb
+++ app/controllers/establishments_controller.rb
@@ -2,17 +2,5 @@ class EstablishmentsController < ApplicationController
 
   def show
     @establishment = Establishment.find(params[:id])
-    respond_to do |format|
-      format.html
-      format.json { render json: @establishment }
-    end
-  end
-
-  def reviews
-    @review = Review.where(establishment_id: params[:establishment][:id])
-    respond_to do |format|
-      format.html
-      format.json { render json: @review }
-    end
   end
 end
