47525257f2a6ac7e39d01e4810d9e62f09dae8c6
Colin Durbin <cddurbin@gmail.com>
2015-03-02 21:57:38 +0000

    Convert form into form helper and confirm creation/write to review model.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,6 +6,18 @@ class ReviewsController < ApplicationController
   end
 
   def create
+    @establishment = Establishment.find(params[:establishment_id])
+    @review = Review.create(review_params)
+    if @review.save
+      redirect_to "/establishments/#{@establishment.id}"
+    else
+      render 'new'
+    end
+  end
+
+private
+  def review_params
+    params.require(:review).permit(:power, :wifi, :food, :noise_level, :overall_rating, :user_id, :establishment_id, :coffee, :content, :service)
   end
 
 end
