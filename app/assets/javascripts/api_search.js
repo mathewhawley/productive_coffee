8cf4581056ef0cf26f1df88a151db0643972a39d
Mathew Hawley <mathew@mathewhawley.com>
2015-03-03 20:31:24 +0000

    Add dynamic name search of API

--- app/assets/javascripts/api_search.js
+++ app/assets/javascripts/api_search.js
@@ -1,15 +1,10 @@
 $(document).ready(function() {
 
-  var apiSearchResults = document.getElementById('api-search-results')
+  var apiSearchResults = document.getElementById('api-search-results');
+
+  var searchInput = document.getElementById('place');
 
   var london = new google.maps.LatLng(51.5072, 0.1275);
-
-  var request = {
-      location: london,
-      name: 'nude',
-      radius: '50000',
-      types: ['cafe']
-  };
 
   function callback(results, status) {
     if (status == google.maps.places.PlacesServiceStatus.OK) {
@@ -23,7 +18,16 @@ $(document).ready(function() {
   var service = new google.maps.places.PlacesService(apiSearchResults);
 
   $('.search-input').on('keypress', function(e) {
-    if (e.which === 13) service.nearbySearch(request, callback)
+    if (e.which === 13) {
+      var request = {
+        location: london,
+        name: searchInput.value,
+        radius: 50000,
+        types: ['cafe']
+      };
+      console.log(searchInput.value);
+      service.nearbySearch(request, callback)
+    }
   })
 
 });
