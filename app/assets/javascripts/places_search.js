0c315a443ab09b29fecfe8c0bde3b6232828d716
Colin Durbin <cddurbin@gmail.com>
2015-03-03 17:08:17 +0000

    Tweak api search.

--- app/assets/javascripts/places_search.js
+++ app/assets/javascripts/places_search.js
@@ -1,17 +1,15 @@
 $(document).ready(function(){
-var places, userPlace, $input = $("#place");
-var autocomplete = new google.maps.places.SearchBox($input[0]);
- 
-google.maps.event.addListener(autocomplete, 'places_changed', function() {
- var places = autocomplete.getPlaces();
- console.log(places);
- if (places.length == 0)
-  return;
- 
- firstPlace = places[0];
- console.log(firstPlace);
- alert(firstPlace.place_id);
-});
+  var places, userPlace, $input = $("#place");
+  var autocomplete = new google.maps.places.SearchBox($input[0]);
+   
+  google.maps.event.addListener(autocomplete, 'places_changed', function() {
+   var places = autocomplete.getPlaces();
+   if (places.length == 0)
+    return;
+   firstPlace = places[0];
+   console.log(places);
+   console.log(firstPlace);
+  });
 
 
 
