271d42ed6c2f1cf149e067ead810276361f684ba
Mathew Hawley <mathew@mathewhawley.com>
2015-03-03 19:11:34 +0000

    Clean up api_search.js – commented code

--- app/assets/javascripts/api_search.js
+++ app/assets/javascripts/api_search.js
@@ -1,28 +1,5 @@
 $(document).ready(function() {
 
-  // var input = $("#search-api").val();
-  // var searchBox = new google.maps.places.SearchBox(input);
-
-  // var service = new google.maps.places.PlacesService((searchInput).val());
-  // console.log(service);
-  
-
-  // $('#search-api').on('keypress', function(e) {
-  //   if (e.which === 13) {
-  //     $.ajax({
-  //       type: 'GET',
-  //       dataType: 'jsonp',
-  //       url: 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyAM4oDKRxYIRC_Oi3lbX9xb5n6GG_j3WQw&location=51.1,-0.06&radius=50000&types=cafe&name=nude'
-  //     }).done(function(response) {
-  //       console.log(response);
-  //     }).fail(function(error) {
-  //       console.log(error);
-  //     })
-  //   }
-  // });
-
-// ---
-
   var apiSearchResults = document.getElementById('api-search-results')
 
   var london = new google.maps.LatLng(51.5072, 0.1275);
