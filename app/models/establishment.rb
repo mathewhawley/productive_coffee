544e8830da835fc15ad6ce59935c0efa170ffc77
Colin Durbin <cddurbin@gmail.com>
2015-03-03 14:31:57 +0000

    Add instance variables calling the average method for remaining attributes.

--- app/models/establishment.rb
+++ app/models/establishment.rb
@@ -9,7 +9,7 @@ class Establishment < ActiveRecord::Base
   # end
 
   def average_ratings(column)
-    values = establishment.reviews.map { |value| value[column] }.compact
+    values = reviews.map { |value| value[column] }.compact
     ((values.inject(:+).to_f)/values.length).round(0)
   end
 
