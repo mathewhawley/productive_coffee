52248187585123ee70d420bace24b45890a53267
Colin Durbin <cddurbin@gmail.com>
2015-03-04 19:33:08 +0000

    Fix user profile update function.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,8 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:sign_up) do |u|
       u.permit(:first_name, :last_name, :occupation, :bio, :profile_pic, :profile_pic_cache, :email, :password, :password_confirmation)
     end
+    devise_parameter_sanitizer.for(:account_update) do |u|
+      u.permit(:first_name, :last_name, :occupation, :bio, :profile_pic, :profile_pic_cache, :email, :password, :password_confirmation, :current_password)
+    end
   end
 end
