fab6aee4d2528bb015012d5de76bea29fcdf0d3e
Colin Durbin <cddurbin@gmail.com>
2015-03-04 11:22:48 +0000

    Remove prior commented out ratings method.

--- app/models/establishment.rb
+++ app/models/establishment.rb
@@ -2,12 +2,6 @@ class Establishment < ActiveRecord::Base
   belongs_to :user
   has_many :reviews
 
-  # def self.rating_overall(params)
-  #   establishment = Establishment.find(params[:id])
-  #   values = establishment.reviews.map { |value| value.overall_rating }.compact
-  #   ((values.inject(:+).to_f)/values.length).round(0)
-  # end
-
   def average_ratings(column)
     values = reviews.map { |value| value[column] }.compact
     ((values.inject(:+).to_f)/values.length).round(0)
