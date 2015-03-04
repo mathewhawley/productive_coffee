3ce3712f97ffa7224c63820a8385792e3ac840ea
Mathew Hawley <mathew@mathewhawley.com>
2015-03-04 12:53:43 +0000

    Display all establishments via Rails. Create links to show pages.

--- app/controllers/establishments_controller.rb
+++ app/controllers/establishments_controller.rb
@@ -2,10 +2,6 @@ class EstablishmentsController < ApplicationController
 
   def index
     @establishments = Establishment.all
-    respond_to do |format|
-      format.html
-      format.json { render json: @establishments }
-    end
   end
 
   def show
