de93ba84cf098e56750095553892d56d11b733dd
Mathew Hawley <mathew@mathewhawley.com>
2015-03-04 14:27:34 +0000

    Tidy, tweak google_maps.js. Log out coordinates of each establishment

--- app/assets/javascripts/google_maps.js
+++ app/assets/javascripts/google_maps.js
@@ -1,29 +1,34 @@
-function initialize() {
-  var mapOptions = {
-    center: new google.maps.LatLng(51.500152, -0.126236),
-    zoom: 13,
-    zoomControl: true,
-    zoomControlOptions: {
-      style: google.maps.ZoomControlStyle.SMALL,
-      position: google.maps.ControlPosition.RIGHT_BOTTOM
-    },
-    panControl: false,
-    streetViewControl: false
-  };
-  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-  // addMarker(map);
-}
-
 $(document).ready(function(){
-  console.log('blah');
-  $.ajax({
-    type: 'GET',
-    url: '/',
-    dataType: 'json'
-  }).done(function(response){
-    console.log(response);
-  });
+
+  function initialize() {
+    var mapOptions = {
+      center: new google.maps.LatLng(51.500152, -0.126236),
+      zoom: 13,
+      zoomControl: true,
+      zoomControlOptions: {
+        style: google.maps.ZoomControlStyle.SMALL,
+        position: google.maps.ControlPosition.RIGHT_BOTTOM
+      },
+      panControl: false,
+      streetViewControl: false
+    };
+    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    addMarker(map);
+  }
+
+  function addMarker(map) {
+    $.ajax({
+      type: 'GET',
+      url: '/',
+      dataType: 'json'
+    }).done(function(response){
+      $.each(response, function(index, establishment){
+        var position = new google.maps.LatLng(establishment.lat, establishment.long);
+        console.log(position);
+      });
+    });
+  }
+
+  google.maps.event.addDomListener(window, 'load', initialize);
 
 });
-
-google.maps.event.addDomListener(window, 'load', initialize);
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,9 @@
 class WelcomeController < ApplicationController
- def index
-  @establishments = Establishment.all
-  respond_to do |format|
-    format.html
-    format.json { render json: @establishments }
+  def index
+    @establishments = Establishment.all
+    respond_to do |format|
+      format.html
+      format.json { render json: @establishments }
+    end
   end
- end
 end
