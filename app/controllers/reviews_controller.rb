905871983e44ea3b79b5d1e68d1389ba7e8f5be0
Colin Durbin <cddurbin@gmail.com>
2015-03-04 16:57:18 +0000

    Add authentication conditions.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,5 @@
 class ReviewsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   
   def new
     @establishment = Establishment.find(params[:establishment_id])
