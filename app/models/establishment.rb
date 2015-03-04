56257be48d8904bd3b32c2376573e7db7f0e96b8
Colin Durbin <cddurbin@gmail.com>
2015-03-04 11:50:56 +0000

    Fix server error and put in conditionals for showing reviews.

--- app/models/establishment.rb
+++ app/models/establishment.rb
@@ -3,8 +3,12 @@ class Establishment < ActiveRecord::Base
   has_many :reviews
 
   def average_ratings(column)
-    values = reviews.map { |value| value[column] }.compact
-    ((values.inject(:+).to_f)/values.length).round(0)
+    if reviews.empty?
+      "Not Reviewed"
+    else
+      values = reviews.map { |value| value[column] }.compact
+      ((values.inject(:+).to_f)/values.length).round(0)
+    end
   end
 
 end
