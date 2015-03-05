2c058f2ec149d71dbd66aa985c7be1cc42458b34
Colin Durbin <cddurbin@gmail.com>
2015-03-05 17:47:54 +0000

    Change ratings class name.

--- app/assets/javascripts/establishment_ratings.js
+++ app/assets/javascripts/establishment_ratings.js
@@ -1,23 +1,49 @@
 $(document).ready(function(){
-  var overall = $('#overall-text').val();
-  console.log(overall);
+  var element = $('#overall')
+  var starIcon = 'fa fa-star fa-1x'
+  // var power = $('#power-text')
+  
+  // <i class="fa fa-power-off"></i>
+  function showRatings(id, value, icon) {
+    for (var i = 1; i <= value; i++) {
 
-  switch(overall) {
-    case 1:
-      $('<li id="overall-rating"><i class="fa fa-star fa-1x"></i>').appendTo('.overall');
-      $('<li id="overall-rating"><i class="fa fa-star-o fa-1x"></i>').appendTo('.overall');
-      $('<li id="overall-rating"><i class="fa fa-star-o fa-1x"></i>').appendTo('.overall');
-      $('<li id="overall-rating"><i class="fa fa-star-o fa-1x"></i>').appendTo('.overall');
-      $('<li id="overall-rating"><i class="fa fa-star-o fa-1x"></i>').appendTo('.overall');
-      break;
-    case 2:
-      $('<li id="overall-rating"><i class="fa fa-star fa-1x"></i>').appendTo('.overall');
-      $('<li id="overall-rating"><i class="fa fa-star fa-1x"></i>').appendTo('.overall');
-      $('<li id="overall-rating"><i class="fa fa-star-o fa-1x"></i>').appendTo('.overall');
-      $('<li id="overall-rating"><i class="fa fa-star-o fa-1x"></i>').appendTo('.overall');
-      $('<li id="overall-rating"><i class="fa fa-star-o fa-1x"></i>').appendTo('.overall');
-      break;
+      console.log(id);
+      console.log(value);
+      console.log(icon);
+
+      $('<li id="'+element.id+'-rating"><i class="'+ starIcon +'"></i>').appendTo(id);
+    }
   }
+  showRatings(element, element.val(), starIcon);
+
+
+  // switch(overall) {
+  //   case 1:
+  //     $('<li id="overall-rating"><i class="fa fa-star fa-1x"></i>').appendTo('.overall');
+  //     break;
+  //   case 2:
+  //     $('<li id="overall-rating"><i class="fa fa-star fa-1x"></i>').appendTo('.overall');
+  //     $('<li id="overall-rating"><i class="fa fa-star fa-1x"></i>').appendTo('.overall');
+  //     break;
+  //   case 3:
+  //     $('<li id="overall-rating"><i class="fa fa-star fa-1x"></i>').appendTo('.overall');
+  //     $('<li id="overall-rating"><i class="fa fa-star fa-1x"></i>').appendTo('.overall');
+  //     $('<li id="overall-rating"><i class="fa fa-star fa-1x"></i>').appendTo('.overall');
+  //     break;
+  //   case 4:
+  //     $('<li id="overall-rating"><i class="fa fa-star fa-1x"></i>').appendTo('.overall');
+  //     $('<li id="overall-rating"><i class="fa fa-star fa-1x"></i>').appendTo('.overall');
+  //     $('<li id="overall-rating"><i class="fa fa-star fa-1x"></i>').appendTo('.overall');
+  //     $('<li id="overall-rating"><i class="fa fa-star fa-1x"></i>').appendTo('.overall');
+  //     break;
+  //   case 5:
+  //     $('<li id="overall-rating"><i class="fa fa-star fa-1x"></i>').appendTo('.overall');
+  //     $('<li id="overall-rating"><i class="fa fa-star fa-1x"></i>').appendTo('.overall');
+  //     $('<li id="overall-rating"><i class="fa fa-star fa-1x"></i>').appendTo('.overall');
+  //     $('<li id="overall-rating"><i class="fa fa-star fa-1x"></i>').appendTo('.overall');
+  //     $('<li id="overall-rating"><i class="fa fa-star fa-1x"></i>').appendTo('.overall');
+  //     break;
+  // }
   
   
 
