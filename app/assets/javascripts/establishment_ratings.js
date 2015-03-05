a20feeb6a0a4785f16fa70126badb8c772f8003e
Colin Durbin <cddurbin@gmail.com>
2015-03-05 19:40:20 +0000

    Finalize icons on cafe page.

--- app/assets/javascripts/establishment_ratings.js
+++ app/assets/javascripts/establishment_ratings.js
@@ -1,62 +1,12 @@
 $(document).ready(function(){
-
   var elements = $('.ratings')
-
   $.each(elements, function(index, element){
-    
-    var id = element.id;
+    var word = element.id;
     var value = element.value;
-    console.log(value);
+    var icon = $(element).attr('data');
 
-    // function showRatings(id, value, icon) {
-    //   for (var i = 1; i <= value; i++) {
-
-    //     console.log(id);
-    //     console.log(value);
-    //     console.log(icon);
-
-    //     $('<li id="'+element.id+'-rating"><i class="'+ starIcon +'"></i>').appendTo(id);
-    //   }
-    // }
+    for (var i = 1; i <= value; i++) {
+      $('<li id="'+word+'-rating"><i class="'+icon+'"></i></li>').appendTo('#'+word+'-ratings');
+    }
   });
-  // var starIcon = 'fa fa-star fa-1x'
-  // // var power = $('#power-text')
   
-  // <i class="fa fa-power-off"></i>
-  
-  // showRatings(element, element.val(), starIcon);
-
-
-  // switch(overall) {
-  //   case 1:
-  //     $('<li id="overall-rating"><i class="fa fa-star fa-1x"></i>').appendTo('.overall');
-  //     break;
-  //   case 2:
-  //     $('<li id="overall-rating"><i class="fa fa-star fa-1x"></i>').appendTo('.overall');
-  //     $('<li id="overall-rating"><i class="fa fa-star fa-1x"></i>').appendTo('.overall');
-  //     break;
-  //   case 3:
-  //     $('<li id="overall-rating"><i class="fa fa-star fa-1x"></i>').appendTo('.overall');
-  //     $('<li id="overall-rating"><i class="fa fa-star fa-1x"></i>').appendTo('.overall');
-  //     $('<li id="overall-rating"><i class="fa fa-star fa-1x"></i>').appendTo('.overall');
-  //     break;
-  //   case 4:
-  //     $('<li id="overall-rating"><i class="fa fa-star fa-1x"></i>').appendTo('.overall');
-  //     $('<li id="overall-rating"><i class="fa fa-star fa-1x"></i>').appendTo('.overall');
-  //     $('<li id="overall-rating"><i class="fa fa-star fa-1x"></i>').appendTo('.overall');
-  //     $('<li id="overall-rating"><i class="fa fa-star fa-1x"></i>').appendTo('.overall');
-  //     break;
-  //   case 5:
-  //     $('<li id="overall-rating"><i class="fa fa-star fa-1x"></i>').appendTo('.overall');
-  //     $('<li id="overall-rating"><i class="fa fa-star fa-1x"></i>').appendTo('.overall');
-  //     $('<li id="overall-rating"><i class="fa fa-star fa-1x"></i>').appendTo('.overall');
-  //     $('<li id="overall-rating"><i class="fa fa-star fa-1x"></i>').appendTo('.overall');
-  //     $('<li id="overall-rating"><i class="fa fa-star fa-1x"></i>').appendTo('.overall');
-  //     break;
-  // }
-  
-  
-
-
-
-});
