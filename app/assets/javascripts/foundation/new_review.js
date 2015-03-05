f1ea17bd87437bd04aa47fc88425cd609e208440
Mathew Hawley <mathew@mathewhawley.com>
2015-03-05 20:44:59 +0000

    Console log hidden field values on submit button click

--- app/assets/javascripts/foundation/new_review.js
+++ app/assets/javascripts/foundation/new_review.js
@@ -1,43 +1,51 @@
 $(document).ready(function(){
 
-  function toggleSelection() {
-
-  }
+  var powerRating;
+  var wifiRating;
+  var coffeeRating;
+  var foodRating;
+  var serviceRating;
+  var noiseRating;
 
   $('.fa-plug').on('click', function(){
     $(this).prevAll().andSelf().addClass('chosen-rating');
     $(this).nextAll().removeClass('chosen-rating');
-    console.log('Power rating: ' + $('.fa-plug.chosen-rating').length);
+    powerRating = $('.fa-plug.chosen-rating').length;
   });
 
   $('.fa-wifi').on('click', function(){
     $(this).prevAll().andSelf().addClass('chosen-rating');
     $(this).nextAll().removeClass('chosen-rating');
-    console.log('Wifi rating: ' + $('.fa-wifi.chosen-rating').length);
+    wifiRating = $('.fa-wifi.chosen-rating').length;
   });
 
   $('.fa-coffee').on('click', function(){
     $(this).prevAll().andSelf().addClass('chosen-rating');
     $(this).nextAll().removeClass('chosen-rating');
-    console.log('Coffee rating: ' + $('.fa-coffee.chosen-rating').length);
+    coffeeRating = $('.fa-coffee.chosen-rating').length;
   });
 
   $('.fa-cutlery').on('click', function(){
     $(this).prevAll().andSelf().addClass('chosen-rating');
     $(this).nextAll().removeClass('chosen-rating');
-    console.log('Food rating: ' + $('.fa-cutlery.chosen-rating').length);
+    foodRating = $('.fa-cutlery.chosen-rating').length;
   });
 
   $('.fa-smile-o').on('click', function(){
     $(this).prevAll().andSelf().addClass('chosen-rating');
     $(this).nextAll().removeClass('chosen-rating');
-    console.log('Service rating: ' + $('.fa-smile-o.chosen-rating').length);
+    serviceRating = $('.fa-smile-o.chosen-rating').length;
   });
 
   $('.fa-volume-up').on('click', function(){
     $(this).prevAll().andSelf().addClass('chosen-rating');
     $(this).nextAll().removeClass('chosen-rating');
-    console.log('Noise rating: ' + $('.fa-volume-up.chosen-rating').length);
+    noiseRating = $('.fa-volume-up.chosen-rating').length;
+  });
+
+  $('.submit').on('click', function(e){
+    console.log($('#establishment-id').val());
+    console.log($('#current-user-id').val());
   });
 
 });
