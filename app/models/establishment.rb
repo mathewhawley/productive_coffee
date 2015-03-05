f58c8e327f33afdfb31fa26ff3db01a6ca448f29
Colin Durbin <cddurbin@gmail.com>
2015-03-05 04:02:19 +0000

    Add unique establishment validation but not right so commented out.

--- app/models/establishment.rb
+++ app/models/establishment.rb
@@ -2,6 +2,8 @@ class Establishment < ActiveRecord::Base
   belongs_to :user
   has_many :reviews
 
+  # validates :address, uniqueness: {message: "Already in DB"}
+
   def average_ratings(column)
     values = reviews.map { |value| value[column] }.compact
     return 'no rating' if values.empty?
