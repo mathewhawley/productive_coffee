29c4faf4969b847ec0382701523f2f86f45230e9
Mathew Hawley <mathew@mathewhawley.com>
2015-03-05 16:52:37 +0000

    Set default cafe image upon creation

--- db/migrate/20150305155509_change_column_default.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColumnDefault < ActiveRecord::Migration
-  def change
-    change_column_default :establishments, :image, 'https://s3-eu-west-1.amazonaws.com/productive-coffee/uploads/cafe/bag-and-hands.jpg'
-  end
-end
