672b6c3c0a07e12459c2d44a3b4199d11f45daab
Colin Durbin <cddurbin@gmail.com>
2015-03-05 16:32:09 +0000

    Format css for overall rating display.

--- app/assets/javascripts/establishment_ratings.js
+++ app/assets/javascripts/establishment_ratings.js
@@ -1,6 +1,25 @@
 $(document).ready(function(){
-  %li
-    %i{class: "fa fa-star fa-3x"}
+  var overall = $('#overall-text').val();
+  console.log(overall);
+
+  switch(overall) {
+    case 1:
+      $('<li id="overall-rating"><i class="fa fa-star fa-1x"></i>').appendTo('.overall');
+      $('<li id="overall-rating"><i class="fa fa-star-o fa-1x"></i>').appendTo('.overall');
+      $('<li id="overall-rating"><i class="fa fa-star-o fa-1x"></i>').appendTo('.overall');
+      $('<li id="overall-rating"><i class="fa fa-star-o fa-1x"></i>').appendTo('.overall');
+      $('<li id="overall-rating"><i class="fa fa-star-o fa-1x"></i>').appendTo('.overall');
+      break;
+    case 2:
+      $('<li id="overall-rating"><i class="fa fa-star fa-1x"></i>').appendTo('.overall');
+      $('<li id="overall-rating"><i class="fa fa-star fa-1x"></i>').appendTo('.overall');
+      $('<li id="overall-rating"><i class="fa fa-star-o fa-1x"></i>').appendTo('.overall');
+      $('<li id="overall-rating"><i class="fa fa-star-o fa-1x"></i>').appendTo('.overall');
+      $('<li id="overall-rating"><i class="fa fa-star-o fa-1x"></i>').appendTo('.overall');
+      break;
+  }
+  
+  
 
 
 
