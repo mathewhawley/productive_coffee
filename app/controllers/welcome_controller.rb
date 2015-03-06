920db6a7513f222fbe755e1cd0e50afda645979c
Mathew Hawley <mathew@mathewhawley.com>
2015-03-06 08:52:32 +0000

    Uncomment most popular

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,11 +3,11 @@ class WelcomeController < ApplicationController
   
   def index
     @establishments = Establishment.all
-    # establishments = Establishment.most_popular(@establishments)
-    # establishment_one = Establishment.find(establishments[0][0])
-    # establishment_two = Establishment.find(establishments[1][0])
-    # establishment_three = Establishment.find(establishments[2][0])
-    # @top_three = [establishment_one, establishment_two, establishment_three]
+    establishments = Establishment.most_popular(@establishments)
+    establishment_one = Establishment.find(establishments[0][0])
+    establishment_two = Establishment.find(establishments[1][0])
+    establishment_three = Establishment.find(establishments[2][0])
+    @top_three = [establishment_one, establishment_two, establishment_three]
     respond_to do |format|
       format.html
       format.json { render json: @establishments }
